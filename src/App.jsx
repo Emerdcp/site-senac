5ca56bc22752eecfc46a89659d6ce5e5d39a2524
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2025-07-03 20:11:49 -0300

    Criação do Projeto Senac, estrutura de pasta
    Criação do Projeto Senac, estrutura de pasta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/icone.png'
+import senacLogo from './assets/senac.png'
 import './App.css'
 
 function App() {
@@ -9,24 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https:/sp.senac.br/americana" target="_blank">
+          <img src={senacLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Site Senac</h1>
+      <h1>Começando com React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador é {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Clique na imagem e conheça nossos curso!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Beba água 💧💧💧
       </p>
     </>
   )
